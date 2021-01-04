cb77b0fa0b590537bed466c5534841865f7807c2
JunhwanKang <wnsghks1017@naver.com>
2021-01-04 16:54:08 +0900

    25일차 공부

---
+++ day25/script/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("header").load("include/header.html");
+    $("footer").load("include/footer.html");
+    $("aside").load("include/aside.html");
+    $("nav").load("include/nav.html");
+})
